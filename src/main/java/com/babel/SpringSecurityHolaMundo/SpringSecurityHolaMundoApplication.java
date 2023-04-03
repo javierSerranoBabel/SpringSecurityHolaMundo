37543fd2500fb8b518759eac9603949187a6d3bf
Javier Serrano <javier.serrano@babelgroup.com>
2023-04-03 09:56:23 +0200

    Initial commit

---
+++ src/main/java/com/babel/SpringSecurityHolaMundo/SpringSecurityHolaMundoApplication.java
@@ -0,0 +1,13 @@
+package com.babel.SpringSecurityHolaMundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityHolaMundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityHolaMundoApplication.class, args);
+	}
+
+}
