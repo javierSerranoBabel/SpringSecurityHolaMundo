b7e987d36c129435753fe943e99be2f9185bbedc
Javier Serrano <javier.serrano@babelgroup.com>
2023-04-03 12:43:11 +0200

    Cifrado contraseño

--- src/main/java/com/babel/SpringSecurityHolaMundo/configuration/SecurityConfiguration.java
+++ src/main/java/com/babel/SpringSecurityHolaMundo/configuration/SecurityConfiguration.java
@@ -1,11 +1,14 @@
 package com.babel.SpringSecurityHolaMundo.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -25,15 +28,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("usuario")
-                .password("{noop}password")
+                .password(passwordEncoder().encode("password"))
                 .roles("USER")
                 .and()
                 .withUser("admin")
-                .password("{noop}password")
+                .password(passwordEncoder().encode("password"))
                 .roles("ADMIN");
 
+    }
+    @Bean
+    protected static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
